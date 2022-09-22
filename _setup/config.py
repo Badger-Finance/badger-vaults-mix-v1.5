810a68757b6eb02d1c242cc8a369b3201c2d895d
Entreprenerd <alex@entreprenerd.xyz>
2022-09-22 17:33:54 +0200

    fix: registry address

--- _setup/config.py
+++ _setup/config.py
@@ -6,7 +6,7 @@ WHALE_ADDRESS = "0xbf72da2bd84c5170618fbe5914b0eca9638d5eb5"
 
 ## Address for Badger Registry, used to fill in default addresses
 ## See: https://github.com/Badger-Finance/badger-registry
-REGISTRY = "0xFda7eB6f8b7a9e9fCFd348042ae675d1d652454f"
+REGISTRY = "0xdc602965F3e5f1e7BAf2446d5564b407d5113A06"
 
 PERFORMANCE_FEE_GOVERNANCE = 2_000 ## 20%
 PERFORMANCE_FEE_STRATEGIST = 0 ## 0%
